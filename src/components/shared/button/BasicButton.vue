49d56a7bdd724e50fc45d572ee464761d7a0b369
Finn Pham <finn.pham@1bit.com.ph>
2024-10-30 20:39:52 +0700

    Update auto play for swipe

--- src/components/shared/button/BasicButton.vue
+++ src/components/shared/button/BasicButton.vue
@@ -6,7 +6,7 @@
       v-slot="{ href, route, navigate, isActive, isExactActive }"
       custom
     >
-      <a :href="href" @click="navigate" target="_blank"
+      <a :href="href" @click="navigate"
         :class="[isActive && 'router-link-active', isExactActive && 'router-link-exact-active', 
         'buttonFi']"
       >
