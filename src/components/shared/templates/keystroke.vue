0e32c5275da76ee4d8b7dc0ec37df1fde6da2b5a
Finn Pham <finn.pham@1bit.com.ph>
2025-01-04 14:07:00 +0700

    A3

--- src/components/shared/templates/keystroke.vue
+++ src/components/shared/templates/keystroke.vue
@@ -19,6 +19,7 @@ const result = ref('');
 
   A1
   A2
+  A3
   <p class="c9"><span class="c3">Technologies used in project: </span></p>
   <p class="c7"><span class="c5">● </span><span class="c2">HTML, CSS, JavaScript.  </span></p>
   <p class="c8"><span class="c5">● </span><span class="c2">Python. </span></p>
