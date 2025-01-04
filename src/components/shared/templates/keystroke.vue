11eeef295384aba2e234490f56de5a297a9d2aa9
Finn Pham <finn.pham@1bit.com.ph>
2025-01-04 13:21:17 +0700

    A4

--- src/components/shared/templates/keystroke.vue
+++ src/components/shared/templates/keystroke.vue
@@ -25,6 +25,7 @@ const result = ref('');
   A1
   A2
   A3
+  A4
   <p class="c39"><span class="c3">Achievements: </span></p>
   <p class="c4"><span class="c5">● </span><span class="c2">JavaScript SDK</span></p>
 <p class="c4"><span class="c5">● </span><span class="c2">Gulp</span></p>
